805eea34627e6bd39511125eb9a7cae46eb62347
Deepak Kumar <deepak.kumar@wowmakers.com>
2013-08-16 11:23:07 +0530

    todo app + login, register, forgot password views

--- public/javascripts/routes.js
+++ public/javascripts/routes.js
@@ -3,19 +3,21 @@ define(
 	"require",
 	"backbone",
 	"views/task_view",
-	"views/home_view"
+	"views/home_view",
+	"views/form_view"
     ],
-    function(require, Backbone, TaskView, HomeView) {
+    function(require, Backbone, TaskView, HomeView, FormView) {
 
 	console.log("router");
 	var Router = Backbone.Router.extend({
 	    routes: {
 		"": "home",
+		"login": "login",
+		"register": "register",
+		"forgot_password": "forgot_password",
 		"tasks": "tasks"
 	    }
 	});
-
-	var TaskListView = TaskView.task_list_view;
 
 	var router = new Router();
 	router.on("route:home", function(){
@@ -23,7 +25,27 @@ define(
 	    homePage.render();
 	});
 
+	router.on("route:login", function(){
+	    var LoginFormView = FormView.login_form_view;
+	    var form = new LoginFormView();
+	    form.render();
+	});
+
+	router.on("route:register", function(){
+	    var RegisterFormView = FormView.register_form_view;
+	    var form = new RegisterFormView();
+	    form.render();
+	});
+
+	router.on("route:forgot_password", function(){
+	    var ForgotFormView = FormView.forgot_form_view;
+	    var form = new ForgotFormView();
+	    form.render();
+	});
+
+
 	router.on("route:tasks", function() {
+	    var TaskListView = TaskView.task_list_view;
 	    var taskList = new TaskListView();
 	    taskList.render();
 	});
